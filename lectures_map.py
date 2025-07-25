98d443909b1bad2ced92e2eed957c5388d874510
mahmoud adil <mahmod.adil2001@gmail.com>
2025-07-26 02:54:08 +0300

    Create lectures_map.py

---
+++ lectures_map.py
@@ -0,0 +1,10 @@
+# lectures_map.py
+
+lectures_names = {
+    1: "Basic Anatomy",
+    2: "Oral Pathology",
+    3: "Dental Materials",
+    4: "Endodontics",
+    5: "Prosthodontics",
+    # زيد أسماء المحاضرات اللي عندك
+}
